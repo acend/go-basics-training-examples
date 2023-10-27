817c96df1fb0844f484c285b6e0278de93e1d80a
jyniybinc <1837725+jyniybinc@users.noreply.github.com>
2023-10-27 07:40:42 +0200

    Allow adding new jokes

--- joke-api-client/main.go
+++ joke-api-client/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -19,10 +22,21 @@ type joke struct {
 func main() {
 	var delay int
 	var url string
+	var addMode bool
 	flag.IntVar(&delay, "delay", 2, "Specify a delay for the punchline")
 	flag.StringVar(&url, "url", "https://official-joke-api.appspot.com/random_joke", "The URL that will be requested")
+	flag.BoolVar(&addMode, "add", false, "Add a new joke")
 	flag.Parse()
 
+	if addMode {
+		err := addJoke(url)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		// Exit the program, because we are in "add mode"
+		os.Exit(0)
+	}
 	fmt.Println("Requesting joke from", url)
 	joke, err := requestJoke(url)
 	if err != nil {
@@ -50,4 +64,44 @@ func requestJoke(url string) (joke, error) {
 		return joke{}, fmt.Errorf("Error decoding body: %w\n", err)
 	}
 	return j, nil
+}
+
+func readJokeFromStdin() (joke, error) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Joke Setup: ")
+	setupInput, err := reader.ReadString('\n')
+	if err != nil {
+		return joke{}, err
+	}
+	fmt.Print("Punchline: ")
+	punchlineInput, err := reader.ReadString('\n')
+	if err != nil {
+		return joke{}, err
+	}
+
+	joke := joke{
+		Setup:     strings.TrimSpace(setupInput),
+		Punchline: strings.TrimSpace(punchlineInput),
+	}
+	return joke, nil
+}
+
+func addJoke(url string) error {
+	joke, err := readJokeFromStdin()
+	if err != nil {
+		return err
+	}
+
+	data, err := json.Marshal(joke)
+	if err != nil {
+		return err
+	}
+	res, err := http.Post(url, "application/json", bytes.NewBuffer(data))
+	if err != nil {
+		return err
+	}
+	if res.StatusCode != http.StatusCreated {
+		return fmt.Errorf("Unexpected status code. got: %v want: 201", res.StatusCode)
+	}
+	return nil
 }
